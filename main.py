e7a27636d510b3226d35671f7de739c7c2a7502a
Artjoms Fedins <tjomaf2018@gmail.com>
2025-07-27 23:05:17 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+from random import randint
+
+pygame.init()
+
+WIDTH = 2560
+HEIGHT = 1440
+
+FPS = 120
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+pygame.display.set_caption("Choosing Game")
+
+clock = pygame.time.Clock()
+run = True
+fullscreen = True
+
+def handle_keyinputs():
+    global fullscreen, screen, run
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_F11:
+            fullscreen = not fullscreen
+            if fullscreen:
+                screen = pygame.display.set_mode((1920, 1080), pygame.RESIZABLE)
+            else:
+                screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+        if event.key == pygame.K_q:
+            run = False
+
+main_color = (32, 32, 32)
+
+while run:
+    screen.fill(main_color)
+    for event in pygame.event.get():
+        handle_keyinputs()
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
