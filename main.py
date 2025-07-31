75dce3d0a92316381bad5995531bca8ccc954a61
Artjoms Fedins <tjomaf2018@gmail.com>
2025-07-31 16:28:59 +0300

    added 2 buttons and a timeline

--- main.py
+++ main.py
@@ -1,18 +1,74 @@
 import pygame
 
+from Button import Button
+
 pygame.init()
 
-WIDTH = 2560
-HEIGHT = 1440
+WIDTH = 1900
+HEIGHT = 900
 
 FPS = 120
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Choosing Game")
 
 clock = pygame.time.Clock()
 run = True
-fullscreen = True
+fullscreen = False
+
+iteration = 0
+MAX_ITERATION = 6
+timeline = []
+
+def left_btn_click():
+    global iteration, MAX_ITERATION, button_left, button_right, timeline
+
+    if not iteration >= MAX_ITERATION:
+        iteration += 1
+        timeline.append(0)
+        if iteration >= MAX_ITERATION:
+            button_left.text = "Die. (Reset)"
+            button_right.text = "Die. (Reset)"
+    else:
+        print(f"You died! Your timeline: {timeline}")
+        iteration = 0
+        timeline = []
+        button_left.text = "Choice 0"
+        button_right.text = "Choice 1"
+
+
+button_left = Button(
+        scr=screen, rect=pygame.Rect(200, HEIGHT//2-100, 600, 200), color=[145, 20, 15], on_click_func=left_btn_click,
+        image_path=None, btn_text="Choice 1", btntext_color=[0, 0, 0],
+        font_path="files/DynaPuff-VariableFont_wdth,wght.ttf", text_size=70, border_thickness=15,
+        border_color=[120, 5, 0], corner_radius=20
+)
+
+def right_btn_click():
+    global iteration, MAX_ITERATION, button_right, button_left, timeline
+
+    if not iteration >= MAX_ITERATION:
+        iteration += 1
+        timeline.append(1)
+        if iteration >= MAX_ITERATION:
+            button_left.text = "Die. (Reset)"
+            button_right.text = "Die. (Reset)"
+    else:
+        print(f"You died! Your timeline: {timeline}")
+        iteration = 0
+        timeline = []
+        button_left.text = "Choice 0"
+        button_right.text = "Choice 1"
+
+
+button_right = Button(
+    scr=screen, rect=pygame.Rect(WIDTH-800, HEIGHT//2-100, 600, 200), color=[145, 20, 15], on_click_func=right_btn_click,
+    image_path=None, btn_text="Choice 2", btntext_color=[0, 0, 0],
+    font_path="files/DynaPuff-VariableFont_wdth,wght.ttf", text_size=70, border_thickness=15,
+    border_color=[120, 5, 0], corner_radius=20
+)
+
+buttons = [button_left, button_right]
 
 def handle_keyinputs():
     global fullscreen, screen, run
@@ -30,6 +86,9 @@ main_color = (32, 32, 32)
 
 while run:
     screen.fill(main_color)
+    for button in buttons:
+        button.handle()
+
     for event in pygame.event.get():
         handle_keyinputs()
         if event.type == pygame.QUIT:
